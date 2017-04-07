526a9214a77064d16fdd58f6c54e6ac5fd18450a
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-04-07 17:21:24 +1200

    Point mutation

--- lib/SexGoodForBits/Genome.hs
+++ lib/SexGoodForBits/Genome.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module SexGoodForBits.Genome where
 
 -- Imports
 import Control.Monad.Primitive (RealWorld)
 import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
 import System.Random.MWC
 
 -- Represent genomes by unboxed vectors of ints
@@ -13,6 +16,8 @@ data Genome = Genome {
                        genes :: U.Vector Int
                      } deriving (Eq, Read, Show)
 
+
+
 -- Generate a random genome (from a uniform distribution
 -- over possible genomes) of the given range and length.
 generateGenome :: Int
@@ -20,6 +25,26 @@ generateGenome :: Int
                -> Gen RealWorld
                -> IO Genome
 generateGenome theRange theLength rng = do
-  theGenes <- U.replicateM theLength $ uniformR (0, theRange) rng
+  theGenes <- U.replicateM theLength $ uniformR (0, theRange-1) rng
   return $ Genome theRange theGenes
+
+
+-- Mutate a genome at a single location
+mutate :: Genome
+       -> Gen RealWorld
+       -> IO Genome
+mutate (Genome {..}) rng = do
+
+  -- The location to mutate
+  location <- uniformR (0, U.length genes - 1) rng
+
+  -- The new value
+  newValue <- uniformR (0, range-1) rng
+
+  -- Put the new value in its place
+  genes' <- U.unsafeThaw genes
+  _ <- UM.unsafeWrite genes' location newValue
+  genes'' <- U.unsafeFreeze genes'
+
+  return $ Genome range genes''
 
--- src/Main.hs
+++ src/Main.hs
@@ -7,8 +7,13 @@ import System.Random.MWC
 -- Main action
 main :: IO ()
 main = withSystemRandom . asGenIO $ \rng -> do
+
   -- Generate and print a genome.
-  genome <- generateGenome 1 10 rng
+  genome <- generateGenome 10 10 rng
   print genome
+
+  -- Mutate it
+  genome' <- mutate genome rng
+  print genome'
 
 
