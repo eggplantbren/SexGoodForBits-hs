c94197a9e06af8329de94f0a90c8ac27573a01c6
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-04-08 11:00:42 +1200

    choose function

--- lib/SexGoodForBits/Population.hs
+++ lib/SexGoodForBits/Population.hs
@@ -7,7 +7,8 @@ import SexGoodForBits.Genome
 import System.Random.MWC
 
 -- Type for a Population
-data Population = Population (V.Vector Genome)
+data Population = Population { populationAge :: Int,
+                               populationGenomes :: V.Vector Genome }
     deriving (Eq, Read, Show)
 
 -- Generate an initial population
@@ -18,11 +19,35 @@ generatePopulation :: Int
 generatePopulation populationSize (genomeRange, genomeLength) rng = do
   let generateOne = generateGenome genomeRange genomeLength rng
   genomes <- V.replicateM populationSize generateOne
-  return $ Population genomes
+  return $ Population 0 genomes
 
 -- Evaluate given fitness function for each genome
 fitnesses :: (Genome -> Double)
           -> Population
           -> V.Vector Double
-fitnesses fitnessFunction (Population gs) = V.map fitnessFunction gs
+fitnesses fitnessFunction (Population _ gs) = V.map fitnessFunction gs
+
+-- Choose a genome from the population, with
+-- more probability for selecting good ones.
+choose :: V.Vector Double       -- Vector of fitnesses
+       -> Gen RealWorld
+       -> IO Int
+choose fs rng = do
+  let n = V.length fs
+  k <- uniformR (0, n-1) rng :: IO Int
+
+  -- Does the k'th fitness beat each other one?
+  let beaten = V.map
+               (\f -> if (fs V.! k) >= f then 1 else 0)
+               fs :: V.Vector Int
+
+  let pAccept = fromIntegral (V.sum beaten) / (fromIntegral n)
+  u <- uniform rng :: IO Double
+
+  if u < pAccept then return k else choose fs rng
+
+---- Update population
+--update :: Population -> Gen RealWorld -> IO Population
+--update population rng = do
+--  
 
