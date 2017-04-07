4845c52623d8cc85682325cb97a43237bf50497f
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-04-07 16:55:13 +1200

    Can generate

--- lib/SexGoodForBits/Genome.hs
+++ lib/SexGoodForBits/Genome.hs
@@ -1,5 +1,25 @@
 module SexGoodForBits.Genome where
 
-test :: Int
-test = 3
+-- Imports
+import Control.Monad.Primitive (RealWorld)
+import qualified Data.Vector.Unboxed as U
+import System.Random.MWC
+
+-- Represent genomes by unboxed vectors of ints
+-- restricted to {0, 1, 2, ..., range - 1}
+-- E.g. a binary genome would have range = 1.
+data Genome = Genome {
+                       range :: Int,
+                       genes :: U.Vector Int
+                     } deriving (Eq, Read, Show)
+
+-- Generate a random genome (from a uniform distribution
+-- over possible genomes) of the given range and length.
+generateGenome :: Int
+               -> Int
+               -> Gen RealWorld
+               -> IO Genome
+generateGenome theRange theLength rng = do
+  theGenes <- U.replicateM theLength $ uniformR (0, theRange) rng
+  return $ Genome theRange theGenes
 
--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,13 @@ module Main where
 
 -- Imports
 import SexGoodForBits.Genome
+import System.Random.MWC
 
+-- Main action
 main :: IO ()
-main = do
-  putStrLn "hello world"
-  print test
+main = withSystemRandom . asGenIO $ \rng -> do
+  -- Generate and print a genome.
+  genome <- generateGenome 1 10 rng
+  print genome
+
 
