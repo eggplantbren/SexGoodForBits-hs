089f0092888cf984feb3614a7c218959479524ea
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-04-08 11:10:08 +1200

    Demonstrate selection of higher fitness genomes

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 -- Imports
+import Data.List (sort)
+import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as U
 import SexGoodForBits.Genome
 import SexGoodForBits.Population
@@ -17,8 +19,15 @@ main = withSystemRandom . asGenIO $ \rng -> do
   let fitnessFunction (Genome _ gs) = fromIntegral (U.sum gs)
 
   -- Evaluate fitnesses
-  let fs = fitnesses fitnessFunction population
-  print fs
+  let fs      = fitnesses fitnessFunction population
+      fsList  = V.toList fs
+      sorted  = sort fsList
+      strings = map (\s -> show s ++ "\n") sorted
+      string  = mconcat strings
+
+  putStrLn string
+  k <- choose fs rng
+  print (fs V.! k)
 
   return ()
 
